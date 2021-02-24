001624c88cbe1136821a990cea6cc71c4c899581
Christian Marquez <christianmarquez41@gmail.com>
2021-02-24 18:08:02 -0500

    added loggin form

--- src/firebase.js
+++ src/firebase.js
@@ -10,3 +10,6 @@ const app = firebase.initializeApp({
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 })
+
+export const auth = app.auth();
+export default app
