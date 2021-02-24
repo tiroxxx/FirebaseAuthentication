3a1b65ff35a949836762a8767ed86e96c87d8734
Christian Marquez <christianmarquez41@gmail.com>
2021-02-24 18:22:09 -0500

    adding authentication from firebase

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { auth } from '../firebase'
 
 const AuthContext = React.createContext()
 
-export function AuthProviuder() {
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProviuder({ children }) {
+    const [currentUser, setCurrentUser] = useState()
+
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password)
+    }
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user)
+        })
+
+        return unsubscribe
+    }, [])
+
+    const value = {
+        currentUser
+    }
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
     )
 }
