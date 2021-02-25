1a86f4ca125399f2a6d889103109b18f1444ed02
Christian Marquez <christianmarquez41@gmail.com>
2021-02-24 20:07:20 -0500

    added login function in the auth context

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
 
     const emailRef = useRef()
     const passwordRef = useRef()
-    const { signup } = useAuth()
+    const { login } = useAuth()
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
 
@@ -17,10 +17,10 @@ export default function Login() {
         try {
             setError("");
             setLoading(true)
-            await signup(emailRef.current.value, passwordRef.current.value)
+            await login(emailRef.current.value, passwordRef.current.value)
         }
         catch {
-            setError("Failed to create an account")
+            setError("Failed to Log In")
         }
 
         setLoading(false)
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -15,6 +15,10 @@ export function AuthProvider({ children }) {
         return auth.createUserWithEmailAndPassword(email, password)
     }
 
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password)
+    }
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
