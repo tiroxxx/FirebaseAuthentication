a85fc84af227d559101e87fcc88845806e0ee6b0
Christian Marquez <christianmarquez41@gmail.com>
2021-02-24 20:28:25 -0500

    added private route

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,24 @@
 import React, { useState } from 'react'
 import { Card, Button, Alert } from "react-bootstrap"
+import { Link, useHistory } from 'react-router-dom';
+import { useAuth } from "../contexts/AuthContext"
 
 export default function Dashboard() {
 
     const [error, setError] = useState("");
+    const { currentUser, logout } = useAuth()
+    const history = useHistory()
 
-    function handleLogout() {
+    async function handleLogout() {
+        setError("")
 
+        try {
+            await logout()
+            history.pushState("/login")
+        }
+        catch {
+            setError("Failed to log out")
+        }
     }
 
     return (
@@ -15,6 +27,8 @@ export default function Dashboard() {
                 <Card.Body>
                     <h2 className="text-center mb-4">Welcome</h2>
                     {error && <Alert variant="danger">{error}</Alert>}
+                    <strong>Email: </strong>{currentUser.email}
+                    <Link to="/update-profile" className="btn btn-primate w-100 mt-3">Update Profile</Link>
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
